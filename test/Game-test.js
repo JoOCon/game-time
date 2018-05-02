4ede2c47c89596728112e2be18ab0c32c50c7c94
Joel O'Connor <joconno07@gmail.com>
2018-05-02 17:29:41 -0600

    Changed colors to RGBA for background

--- test/Game-test.js
+++ test/Game-test.js
@@ -5,11 +5,8 @@ describe('Game test suiet', () => {
   it('should instantiante a new Game state', () => {
     //setup
     const game = new Game();
-
-
     //execution
     const gameState = { lives: 3, gameActive: true, level: 0};
-
     //expect
     expect(game).to.deep.equal(gameState);
   });
