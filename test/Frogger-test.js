7467ad1372a37f300dab43e7d9a797ab99ca0a07
Andrew James <ajames20@gmail.com>
2018-05-02 14:16:09 -0600

    Add Charecter, Frogger Class and Frogger Test

---
+++ test/Frogger-test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const Frogger = require('../lib/Frogger.js');
+
+
+describe('Card', () => {
+  // Setup
+  const frog = new Frogger(200, 200, 20, 20);
+  // Execution
+
+  // Expect
+});
