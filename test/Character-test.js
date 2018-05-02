65c2e41c345d06e09a2fb5eb11a10b46a372a240
Andrew James <ajames20@gmail.com>
2018-05-02 14:21:06 -0600

    I learned how to spell Character so I updated it

---
+++ test/Character-test.js
@@ -0,0 +1,11 @@
+const { expect } = require('chai');
+const Frogger = require('../lib/Frogger.js');
+
+
+describe('Character', () => {
+  // Setup
+  const frog = new Frogger(200, 200, 20, 20);
+  // Execution
+
+  // Expect
+});
