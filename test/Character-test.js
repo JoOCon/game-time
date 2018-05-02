6caa850e555e755d8d69c0723842bb8c3c79dc20
Andrew James <ajames20@gmail.com>
2018-05-02 14:39:30 -0600

    Add locus

--- test/Character-test.js
+++ test/Character-test.js
@@ -2,9 +2,13 @@ const { expect } = require('chai');
 const Character = require('../lib/Character');
 
 describe('Character', () => {
-  // Setup
-  const character = new Character();
-  // Execution
+  it('should instantiate a new Character with a x pos y pos width and height value', function () {
+    // Setup
+    const character = new Character(0, 10, 50, 50);
+    // Execution
+    const expected = { x: 0, y: 10, width: 50, height: 50 };
+    // Expect
 
-  // Expect
+    expect(character).to.deep.equal(expected);
+  });
 });
