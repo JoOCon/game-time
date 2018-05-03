85d9dc660a754d8f69c00071d9fa880ba69d569b
Andrew James <ajames20@gmail.com>
2018-05-03 15:19:43 -0600

    Add Log Class and testing file

---
+++ test/Log-test.js
@@ -0,0 +1,13 @@
+const { expect } = require('chai');
+const Log = require('../lib/Log.js');
+
+describe('Game test suite', () => {
+  it('should instantiante Logs at the correct coordinates', () => {
+    // Setup
+
+    // Execution
+
+    // Expectation
+
+  });
+});
