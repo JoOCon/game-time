272382c21fb4d3b0db24451fa8aefc6f20a11685
Andrew James <ajames20@gmail.com>
2018-05-03 09:04:28 -0600

    Add keyboard input movement to Frogger and test to check if movemnt worked

--- test/Frogger-test.js
+++ test/Frogger-test.js
@@ -5,20 +5,26 @@ require('locus');
 describe('Frogger test suite', () => {
   it('should insatiate a new Frogger int the correct position', () => {
     // Setup
-    const frog = new Frogger(200, 200, 20, 20);
+    const frog = new Frogger(275, 650, 50, 50);
     // Execution
-    const expected = { x: 200, y: 200, width: 20, height: 20 };
+    const expected = { x: 275, y: 650, width: 50, height: 50 };
     // Expect
 
     expect(frog).to.deep.equal(expected);
   });
 
-  it.skip('should be able to move by 50px up', () => {
-    const frog = new Frogger(200, 650, 20, 20);
+  it.only('should be able to move by 50px up/down || 68px left/right', () => {
+    const frog = new Frogger(275, 650, 50, 50);
+    // const event = 'keyup';
+    const event = 'up';
     const expected = 600;
     // Execution
-    const frogMove = frog.move();
 
-    expect(frogMove).to.be(expected);
+    frog.move(event);
+    const actual = frog.y;
+
+    // eval(locus);
+
+    expect(actual).to.equal(expected);
   });
 });
