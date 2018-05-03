c2fed58b7575cea8765b34a0012b8621021b534b
Joel O'Connor <joconno07@gmail.com>
2018-05-03 16:36:45 -0600

    Made change to game.js so Autos are now being generated there instead of on index.js

--- test/Auto-test.js
+++ test/Auto-test.js
@@ -1,12 +1,31 @@
 const { expect } = require('chai');
 const Auto = require('../lib/Auto.js');
+require('locus');
 
 describe('Auto test suite', () => {
   //setup
-  it('should create a new Auto', () => {
-    const car = new Auto();
+  it('When Auto is instanciated it should receive an object as its params and set color to red', () => {
+    const params = { x: 50, y: 50, width: 100, 
+                     height: 100, autoType: null,
+                    color: 'red' }
+    const car = new Auto(params);
+
+    const expectedColor = 'red'
+
+    expect(car.color).to.equal(expectedColor)
+
   });
-  //execution
-  
-  //expect
+
+  it('when the move method is invoked i expect the x of auto to increment by 1', () => {
+    const expectedX = 51;
+    const params = { x: 50, y: 50, width: 100, 
+                     height: 100, autoType: null,
+                    color: 'red' }
+    const car = new Auto(params);
+
+    const actual = car.move()
+
+    expect(actual.x).to.equal(expectedX)
+  })
+
 });
