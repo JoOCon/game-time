21dd92557657e0ffeed2173d6e90db2234e96594
Andrew James <ajames20@gmail.com>
2018-05-04 09:53:52 -0600

    Update Character constructor to lcontrol drawing

--- test/Log-test.js
+++ test/Log-test.js
@@ -2,12 +2,18 @@ const { expect } = require('chai');
 const Log = require('../lib/Log.js');
 
 describe('Log test suite', () => {
-  it('should instantiate Logs at the correct coordinates', () => {
+  it('should instantiate a Log at the correct coordinates', () => {
     // Setup
-
+    const log = new Log('../img/log.png', 0, 295, 225, 55);
     // Execution
-
+    const expected = {
+      img: '../img/log.png',
+      x: 0, y: 295,
+      width: 225,
+      height: 55
+    };
     // Expectation
 
+    expect(log).to.deep.equal(expected);
   });
 });
