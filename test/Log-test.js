1f8e604ce787b05b5624fea55afa3e97073429d1
Andrew James <ajames20@gmail.com>
2018-05-03 21:14:01 -0600

    Update Log test suite name

--- test/Log-test.js
+++ test/Log-test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const Log = require('../lib/Log.js');
 
-describe('Game test suite', () => {
+describe('Log test suite', () => {
   it('should instantiante Logs at the correct coordinates', () => {
     // Setup
 
