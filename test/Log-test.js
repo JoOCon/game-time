493931b83d771da8b896a33ffe4192747a7f594f
Andrew James <ajames20@gmail.com>
2018-05-04 09:25:44 -0600

    Update eslint.json

--- test/Log-test.js
+++ test/Log-test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 const Log = require('../lib/Log.js');
 
 describe('Log test suite', () => {
-  it('should instantiante Logs at the correct coordinates', () => {
+  it('should instantiate Logs at the correct coordinates', () => {
     // Setup
 
     // Execution
