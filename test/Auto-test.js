aae4505ba14ef05cd14537e4095a5979589489c8
Joel O'Connor <joconno07@gmail.com>
2018-05-04 15:49:37 -0600

    added in feature so cars will start back at x=0 once they reach the end of screen

--- test/Auto-test.js
+++ test/Auto-test.js
@@ -1,20 +1,18 @@
 const { expect } = require('chai');
 const Auto = require('../lib/Auto.js');
-require('locus');
+const Game = require('../lib/Game.js');
+// require('locus');
 
 describe('Auto test suite', () => {
   //setup
-  it('When Auto is instanciated it should receive an object as its params and set color to red', () => {
-    const params = { x: 50, y: 50, width: 100, 
-                     height: 100, autoType: null,
-                    color: 'red' }
-    const car = new Auto(params);
+  // it('When a new Auto is istantiated it should have a size', () => {
+  //   const params = { width: 100, height: 40 };
+  //   const car = new Auto(params);
 
-    const expectedColor = 'red'
+  //   const expectedSize = 100;
 
-    expect(car.color).to.equal(expectedColor)
-
-  });
+  //   expect(car.width).to.equal(expectedSize);
+  // });
 
   it('when the move method is invoked i expect the x of auto to increment by 1', () => {
     const expectedX = 51;
@@ -23,9 +21,9 @@ describe('Auto test suite', () => {
                     color: 'red' }
     const car = new Auto(params);
 
-    const actual = car.move()
+    const actual = car.move();
 
-    expect(actual.x).to.equal(expectedX)
+    expect(actual.x).to.equal(expectedX);
   })
 
 });
