3d0e53205feed40a9854d31a468f7dcb747f3f13
Andrew James <ajames20@gmail.com>
2018-05-05 08:42:06 -0600

    Move character generation into Game and add tests coverage for log movment

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,14 +1,42 @@
 const { expect } = require('chai');
 const Game = require('../lib/Game.js');
+const Log = require('../lib/Log.js');
 
 describe('Game test suite', () => {
   it('should instantiate a new Game state', () => {
     //setup
-    const game = new Game();
+    const frogImg = '../img/frogger.png';
+    const logImg = '../img/log.png';
+    const game = new Game(frogImg, logImg);
     //execution
-    const gameState = { lives: 3, gameActive: true, level: 0 };
+    const gameState = { lives: 3, gameActive: true, level: 0, frogImg, logImg };
     //expect
 
     expect(game).to.deep.equal(gameState);
+  });
+
+  it('should make an object of arrays to hold all logs', () => {
+    // Setup
+    const frogImg = '../img/frogger.png';
+    const logImg = '../img/log.png';
+    const game = new Game(frogImg, logImg);
+
+    const log1 = new Log('../img/log.png', -150, 298, 225, 52);
+    const log2 = new Log('../img/log.png', 550, 248, 225, 52);
+    const log3 = new Log('../img/log.png', -150, 198, 225, 52);
+    const log4 = new Log('../img/log.png', 550, 148, 225, 52);
+    const log5 = new Log('../img/log.png', -150, 98, 225, 52);
+    const log6 = new Log('../img/log.png', 550, 48, 225, 52);
+    const expected = {
+      evens: [log1, log3, log5],
+      odds: [log2, log4, log6]
+    };
+
+    // Execution
+    const actual = game.generateLogs();
+    // Expectation
+
+    // eval(locus);
+    expect(actual).to.deep.equal(expected);
   });
 });
