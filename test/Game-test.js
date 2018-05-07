42cc893d43e7de1ae240c2688d50b6240fe8691e
Andrew James <ajames20@gmail.com>
2018-05-07 12:28:05 -0600

    Update generate logs to be in Game class add test to cover changee

--- test/Game-test.js
+++ test/Game-test.js
@@ -11,8 +11,15 @@ describe('Game test suite', () => {
     const autoImg = '../img/yellow-car.png';
     const game = new Game(frogImg, logImg, autoImg);
     const frog = new Frogger(frogImg, 275, 650, 50, 50);
+    const log1 = new Log(logImg, -150, 298, 250, 52, 1.5, 'right');
+    const log2 = new Log(logImg, 550, 248, 250, 52, 2, 'left');
+    const log3 = new Log(logImg, -150, 198, 250, 52, .5, 'right');
+    const log4 = new Log(logImg, 550, 148, 250, 52, 1.5, 'left');
+    const log5 = new Log(logImg, -150, 98, 250, 52, 1, 'right');
+    const log6 = new Log(logImg, 550, 48, 250, 52, 1, 'left');
+    const logs = [log1, log2, log3, log4, log5, log6];
     //execution
-    const gameState = { lives: 3, gameActive: true, level: 0, frogImg, logImg, autoImg, frog };
+    const gameState = { lives: 3, gameActive: true, level: 0, frogImg, logImg, autoImg, frog, logs };
     //expect
 
     expect(game).to.deep.equal(gameState);
